851dc759b2277266f889951ecbd66c2daf44df1a
Patrice Brend'amour <patrice@brendamour.net>
2025-03-01 17:38:01 +0100

    fix: hopefully reverted formatting changes based on editorconfig file

--- server/src/main/kotlin/com/vishnurajeevan/libroabs/Application.kt
+++ server/src/main/kotlin/com/vishnurajeevan/libroabs/Application.kt
@@ -33,82 +33,82 @@ import kotlin.time.Duration.Companion.days
 import kotlin.time.Duration.Companion.hours
 
 fun main(args: Array<String>) {
-    NoOpCliktCommand(name = "librofm-abs")
-        .subcommands(Run())
-        .main(args)
+  NoOpCliktCommand(name = "librofm-abs")
+    .subcommands(Run())
+    .main(args)
 }
 
 class Run : CliktCommand("run") {
-    private val port by option("--port")
-        .int()
-        .default(8080)
+  private val port by option("--port")
+    .int()
+    .default(8080)
 
-    private val dataDir by option("--data-dir")
-        .default("/data")
+  private val dataDir by option("--data-dir")
+    .default("/data")
 
-    private val mediaDir by option("--media-dir")
-        .default("/media")
+  private val mediaDir by option("--media-dir")
+    .default("/media")
 
-    private val syncInterval by option("--sync-interval", envvar = "SYNC_INTERVAL")
-        .choice("h", "d", "w")
-        .default("d")
+  private val syncInterval by option("--sync-interval", envvar = "SYNC_INTERVAL")
+    .choice("h", "d", "w")
+    .default("d")
 
-    private val dryRun by option("--dry-run", "-n", envvar = "DRY_RUN")
-        .flag(default = false)
+  private val dryRun by option("--dry-run", "-n", envvar = "DRY_RUN")
+    .flag(default = false)
 
-    private val renameChapters by option("--rename-chapters", envvar = "RENAME_CHAPTERS")
-        .flag(default = false)
+  private val renameChapters by option("--rename-chapters", envvar = "RENAME_CHAPTERS")
+    .flag(default = false)
 
-    private val writeTitleTag by option("--write-title-tag", envvar = "WRITE_TITLE_TAG")
-        .flag(default = false)
+  private val writeTitleTag by option("--write-title-tag", envvar = "WRITE_TITLE_TAG")
+    .flag(default = false)
 
-    private val convertToM4b by option("--convert-to-m4b", envvar = "CONVERT_TO_M4B")
-        .flag(default = false)
+  private val convertToM4b by option("--convert-to-m4b", envvar = "CONVERT_TO_M4B")
+    .flag(default = false)
 
-    private val ffmpegPath by option("--ffmpeg-path")
-        .default("/usr/bin/ffmpeg")
+  private val ffmpegPath by option("--ffmpeg-path")
+    .default("/usr/bin/ffmpeg")
 
-    private val ffprobePath by option("--ffprobe-path")
-        .default("/usr/bin/ffprobe")
+  private val ffprobePath by option("--ffprobe-path")
+    .default("/usr/bin/ffprobe")
 
-    private val verbose by option("--verbose", "-v", envvar = "VERBOSE")
-        .flag(default = false)
+  private val verbose by option("--verbose", "-v", envvar = "VERBOSE")
+    .flag(default = false)
 
-    // Limits the number of books pulled down to 1
-    private val devMode by option("--dev-mode", "-d", envvar = "DEV_MODE")
-        .flag(default = false)
+  // Limits the number of books pulled down to 1
+  private val devMode by option("--dev-mode", "-d", envvar = "DEV_MODE")
+    .flag(default = false)
 
-    private val libroFmUsername by option("--libro-fm-username", envvar = "LIBRO_FM_USERNAME")
-        .required()
+  private val libroFmUsername by option("--libro-fm-username", envvar = "LIBRO_FM_USERNAME")
+    .required()
 
-    private val libroFmPassword by option("--libro-fm-password", envvar = "LIBRO_FM_PASSWORD")
-        .required()
+  private val libroFmPassword by option("--libro-fm-password", envvar = "LIBRO_FM_PASSWORD")
+    .required()
 
-    private val lfdLogger: (String) -> Unit = {
-        if (verbose) {
-            println(it)
-        }
+  private val lfdLogger: (String) -> Unit = {
+    if (verbose) {
+      println(it)
     }
+  }
 
-    private val libroFmApi by lazy {
-        LibroApiHandler(
-            client = HttpClient { },
-            dataDir = dataDir,
-            dryRun = dryRun,
-            verbose = verbose,
-            lfdLogger = lfdLogger
-        )
-    }
+  private val libroFmApi by lazy {
+    LibroApiHandler(
+      client = HttpClient { },
+      dataDir = dataDir,
+      dryRun = dryRun,
+      verbose = verbose,
+      lfdLogger = lfdLogger
+    )
+  }
 
-    private val m4bUtil by lazy {
-        M4BUtil(ffmpegPath, ffprobePath)
-    }
+  private val m4bUtil by lazy {
+    M4BUtil(ffmpegPath, ffprobePath)
+  }
 
-    private val serverScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+  private val serverScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
 
-    override fun run() {
-        println(
-            """
+  override fun run() {
+    println(
+      """
         Starting up!
         internal port: $port
         syncInterval: $syncInterval
@@ -122,156 +122,156 @@ class Run : CliktCommand("run") {
         libroFmUsername: $libroFmUsername
         libroFmPassword: ${libroFmPassword.map { "*" }.joinToString("")}
       """.trimIndent()
-        )
+    )
 
-        runBlocking {
-            val dataDir = File(dataDir).apply {
-                if (!exists()) {
-                    mkdirs()
-                }
-            }
-            val tokenFile = File("$dataDir/token.txt")
-            if (!tokenFile.exists()) {
-                lfdLogger("Token file not found, logging in")
-                libroFmApi.fetchLoginData(libroFmUsername, libroFmPassword)
-            }
+    runBlocking {
+      val dataDir = File(dataDir).apply {
+        if (!exists()) {
+          mkdirs()
+        }
+      }
+      val tokenFile = File("$dataDir/token.txt")
+      if (!tokenFile.exists()) {
+        lfdLogger("Token file not found, logging in")
+        libroFmApi.fetchLoginData(libroFmUsername, libroFmPassword)
+      }
 
+      libroFmApi.fetchLibrary()
+      processLibrary()
+
+      launch {
+        lfdLogger("Sync Interval: $syncInterval")
+        val syncIntervalTimeUnit = when (syncInterval) {
+          "h" -> 1.hours
+          "d" -> 1.days
+          "w" -> 7.days
+          else -> error("Unhandled sync interval")
+        }
+
+        Clock.System.intervalPulse(syncIntervalTimeUnit)
+          .beat { scheduled, occurred ->
+            lfdLogger("Checking library on pulse!")
             libroFmApi.fetchLibrary()
             processLibrary()
+          }
+      }
 
-            launch {
-                lfdLogger("Sync Interval: $syncInterval")
-                val syncIntervalTimeUnit = when (syncInterval) {
-                    "h" -> 1.hours
-                    "d" -> 1.days
-                    "w" -> 7.days
-                    else -> error("Unhandled sync interval")
+      serverScope.launch {
+        embeddedServer(
+          factory = Netty,
+          port = port,
+          host = "0.0.0.0",
+          module = {
+            routing {
+              post("/update") {
+                call.respondText("Updating!")
+                libroFmApi.fetchLibrary()
+                processLibrary()
+              }
+              post("/convertToM4b/{isbn}") {
+                call.parameters["isbn"]?.let { isbn ->
+                  if (isbn == "all") {
+                    call.respondText("Starting conversion process for all books in the library")
+                    convertAllBooksToM4b()
+                  } else {
+                    call.respondText("Starting conversion process for $isbn")
+                    convertBookToM4b(isbn)
+                  }
                 }
-
-                Clock.System.intervalPulse(syncIntervalTimeUnit)
-                    .beat { scheduled, occurred ->
-                        lfdLogger("Checking library on pulse!")
-                        libroFmApi.fetchLibrary()
-                        processLibrary()
-                    }
+              }
             }
-
-            serverScope.launch {
-                embeddedServer(
-                    factory = Netty,
-                    port = port,
-                    host = "0.0.0.0",
-                    module = {
-                        routing {
-                            post("/update") {
-                                call.respondText("Updating!")
-                                libroFmApi.fetchLibrary()
-                                processLibrary()
-                            }
-                            post("/convertToM4b/{isbn}") {
-                                call.parameters["isbn"]?.let { isbn ->
-                                    if (isbn == "all") {
-                                        call.respondText("Starting conversion process for all books in the library")
-                                        convertAllBooksToM4b()
-                                    } else {
-                                        call.respondText("Starting conversion process for $isbn")
-                                        convertBookToM4b(isbn)
-                                    }
-                                }
-                            }
-                        }
-                    }
-                ).start(wait = true)
-            }
-        }
+          }
+        ).start(wait = true)
+      }
     }
+  }
 
-    private fun getLibrary(): LibraryMetadata {
-        return Json.decodeFromString<LibraryMetadata>(
-            File("$dataDir/library.json").readText()
-        )
-    }
+  private fun getLibrary(): LibraryMetadata {
+    return Json.decodeFromString<LibraryMetadata>(
+      File("$dataDir/library.json").readText()
+    )
+  }
 
-    private suspend fun processLibrary() {
-        val localLibrary = getLibrary()
+  private suspend fun processLibrary() {
+    val localLibrary = getLibrary()
 
-        localLibrary.audiobooks
-            .let {
-                if (devMode) {
-                    it.take(1)
-                } else {
-                    it
-                }
-            }
-            .forEach { book ->
-                val targetDir = File("$mediaDir/${book.authors.first()}/${book.title}")
-
-                if (!targetDir.exists()) {
-                    lfdLogger("downloading ${book.title}")
-                    targetDir.mkdirs()
-                    val downloadData = libroFmApi.fetchDownloadMetadata(book.isbn)
-                    libroFmApi.fetchAudioBook(
-                        data = downloadData.parts,
-                        targetDirectory = targetDir
-                    )
-
-                    if (renameChapters) {
-                        libroFmApi.renameChapters(
-                            title = book.title,
-                            tracks = downloadData.tracks,
-                            targetDirectory = targetDir,
-                            writeTitleTag = writeTitleTag
-                        )
-                    }
-                    if (convertToM4b) {
-                        lfdLogger("Converting ${book.title} from mp3 to m4b.")
-                        m4bUtil.convertBookToM4b(book, targetDir)
-                    }
-                } else {
-                    lfdLogger("skipping ${book.title} as it exists on the filesystem!")
-                }
-            }
-    }
-
-    private suspend fun convertAllBooksToM4b() {
-        val localLibrary = getLibrary()
-
-        localLibrary.audiobooks
-            .let {
-                if (devMode) {
-                    it.take(1)
-                } else {
-                    it
-                }
-            }
-            .forEach { book ->
-                convertBookToM4b(book)
-            }
-    }
-
-    private suspend fun convertBookToM4b(isbn: String) {
-        val localLibrary = getLibrary()
-        val book = localLibrary.audiobooks.find { it.isbn == isbn }
-        if (book == null) {
-            lfdLogger("Book with isbn $isbn not found!")
+    localLibrary.audiobooks
+      .let {
+        if (devMode) {
+          it.take(1)
         } else {
-            convertBookToM4b(book)
+          it
         }
-    }
-
-    private suspend fun convertBookToM4b(book: Book) {
+      }
+      .forEach { book ->
         val targetDir = File("$mediaDir/${book.authors.first()}/${book.title}")
+
         if (!targetDir.exists()) {
-            lfdLogger("Book ${book.title} is not downloaded yet!")
-            return
+          lfdLogger("downloading ${book.title}")
+          targetDir.mkdirs()
+          val downloadData = libroFmApi.fetchDownloadMetadata(book.isbn)
+          libroFmApi.fetchAudioBook(
+            data = downloadData.parts,
+            targetDirectory = targetDir
+          )
+
+          if (renameChapters) {
+            libroFmApi.renameChapters(
+              title = book.title,
+              tracks = downloadData.tracks,
+              targetDirectory = targetDir,
+              writeTitleTag = writeTitleTag
+            )
+          }
+          if (convertToM4b) {
+            lfdLogger("Converting ${book.title} from mp3 to m4b.")
+            m4bUtil.convertBookToM4b(book, targetDir)
+          }
+        } else {
+          lfdLogger("skipping ${book.title} as it exists on the filesystem!")
         }
-        val targetFile = File("$mediaDir/${book.authors.first()}/${book.title}/${book.title}.m4b")
-        if (targetFile.exists()) {
-            lfdLogger("Skipping ${book.title} as it's already converted!")
-            return
+      }
+  }
+
+  private suspend fun convertAllBooksToM4b() {
+    val localLibrary = getLibrary()
+
+    localLibrary.audiobooks
+      .let {
+        if (devMode) {
+          it.take(1)
+        } else {
+          it
         }
-        lfdLogger("Converting ${book.title} from mp3 to m4b.")
-        m4bUtil.convertBookToM4b(book, targetDir)
+      }
+      .forEach { book ->
+        convertBookToM4b(book)
+      }
+  }
+
+  private suspend fun convertBookToM4b(isbn: String) {
+    val localLibrary = getLibrary()
+    val book = localLibrary.audiobooks.find { it.isbn == isbn }
+    if (book == null) {
+      lfdLogger("Book with isbn $isbn not found!")
+    } else {
+      convertBookToM4b(book)
     }
+  }
+
+  private suspend fun convertBookToM4b(book: Book) {
+    val targetDir = File("$mediaDir/${book.authors.first()}/${book.title}")
+    if (!targetDir.exists()) {
+      lfdLogger("Book ${book.title} is not downloaded yet!")
+      return
+    }
+    val targetFile = File("$mediaDir/${book.authors.first()}/${book.title}/${book.title}.m4b")
+    if (targetFile.exists()) {
+      lfdLogger("Skipping ${book.title} as it's already converted!")
+      return
+    }
+    lfdLogger("Converting ${book.title} from mp3 to m4b.")
+    m4bUtil.convertBookToM4b(book, targetDir)
+  }
 }
 
--- server/src/main/kotlin/com/vishnurajeevan/libroabs/libro/M4BUtil.kt
+++ server/src/main/kotlin/com/vishnurajeevan/libroabs/libro/M4BUtil.kt
@@ -14,141 +14,141 @@ import java.nio.file.StandardCopyOption
 import java.util.concurrent.TimeUnit
 
 class M4BUtil(
-    ffmpegPath: String,
-    ffprobePath: String,
-    private val lfdLogger: (String) -> Unit = {},
+  ffmpegPath: String,
+  ffprobePath: String,
+  private val lfdLogger: (String) -> Unit = {},
 ) {
 
-    private val ffmpeg = FFmpeg(ffmpegPath)
-    private val ffprobe = FFprobe(ffprobePath)
+  private val ffmpeg = FFmpeg(ffmpegPath)
+  private val ffprobe = FFprobe(ffprobePath)
 
-    suspend fun convertBookToM4b(book: Book, targetDirectory: File) {
-        val newFile = File(targetDirectory, "${book.title}.m4b")
+  suspend fun convertBookToM4b(book: Book, targetDirectory: File) {
+    val newFile = File(targetDirectory, "${book.title}.m4b")
 
-        // Download Cover Image
-        val coverFile = File(targetDirectory, "cover.jpg")
-        downloadCoverImage(book.cover_url, coverFile)
+    // Download Cover Image
+    val coverFile = File(targetDirectory, "cover.jpg")
+    downloadCoverImage(book.cover_url, coverFile)
 
-        val chapterFiles =
-            targetDirectory.listFiles { file -> file.extension == "mp3" }?.sortedBy { it.nameWithoutExtension }
-                ?: emptyList()
+    val chapterFiles =
+      targetDirectory.listFiles { file -> file.extension == "mp3" }?.sortedBy { it.nameWithoutExtension }
+        ?: emptyList()
 
-        val metadataFile = File(targetDirectory, "metadata.txt")
-        generateMetadataFile(book, chapterFiles, metadataFile)
+    val metadataFile = File(targetDirectory, "metadata.txt")
+    generateMetadataFile(book, chapterFiles, metadataFile)
 
-        // Create a list file to handle spaces in filenames
-        val listFile = File(targetDirectory, "input_list.txt").apply {
-            bufferedWriter().use { writer ->
-                chapterFiles.forEach { file ->
-                    if (!file.exists()) throw IllegalArgumentException("MP3 file not found: ${file.absolutePath}")
-                    writer.write("file '${file.absolutePath}'\n")
-                }
-            }
+    // Create a list file to handle spaces in filenames
+    val listFile = File(targetDirectory, "input_list.txt").apply {
+      bufferedWriter().use { writer ->
+        chapterFiles.forEach { file ->
+          if (!file.exists()) throw IllegalArgumentException("MP3 file not found: ${file.absolutePath}")
+          writer.write("file '${file.absolutePath}'\n")
         }
-
-        val builder = FFmpegBuilder()
-            .addExtraArgs("-f", "concat", "-safe", "0")
-            .addExtraArgs("-i", listFile.absolutePath)
-            .addExtraArgs("-f", "ffmetadata", "-i", metadataFile.absolutePath)
-            .addInput(coverFile.absolutePath)
-            .addOutput(newFile.absolutePath)
-            .setAudioCodec("aac")
-            .addExtraArgs("-b:a", "128k")
-            .addExtraArgs("-movflags", "faststart")
-            .addExtraArgs("-map", "0:a") // Audio from MP3s
-            .addExtraArgs("-map_metadata", "1")
-            .addExtraArgs("-map", "2:v") // Cover image
-            .addExtraArgs("-disposition:v:0", "attached_pic")
-            .addExtraArgs("-c:v:0", "mjpeg")
-            .addExtraArgs("-id3v2_version", "3")
-            .done()
-
-        val executor = FFmpegExecutor(ffmpeg, ffprobe)
-        executor.createJob(builder, object : ProgressListener {
-            val durationNs = book.audiobook_info.duration * TimeUnit.SECONDS.toNanos(1)
-            override fun progress(progress: Progress) {
-                val percentage = progress.out_time_ns.toDouble() / durationNs.toDouble();
-                lfdLogger(
-                    String.format(
-                        "[%.0f%%] status:%s time:%s ms speed:%.2fx",
-                        percentage * 100,
-                        progress.status,
-                        FFmpegUtils.toTimecode(progress.out_time_ns, TimeUnit.NANOSECONDS),
-                        progress.speed
-                    )
-                )
-            }
-        }).run()
-
-        //cleanup
-        listFile.delete()
-        metadataFile.delete()
-
-        lfdLogger("M4B file created: ${newFile.absolutePath}")
+      }
     }
 
-    private fun downloadCoverImage(coverUrl: String, outputFile: File) {
-        val fullUrl = if (coverUrl.startsWith("//")) "https:$coverUrl" else coverUrl
+    val builder = FFmpegBuilder()
+      .addExtraArgs("-f", "concat", "-safe", "0")
+      .addExtraArgs("-i", listFile.absolutePath)
+      .addExtraArgs("-f", "ffmetadata", "-i", metadataFile.absolutePath)
+      .addInput(coverFile.absolutePath)
+      .addOutput(newFile.absolutePath)
+      .setAudioCodec("aac")
+      .addExtraArgs("-b:a", "128k")
+      .addExtraArgs("-movflags", "faststart")
+      .addExtraArgs("-map", "0:a") // Audio from MP3s
+      .addExtraArgs("-map_metadata", "1")
+      .addExtraArgs("-map", "2:v") // Cover image
+      .addExtraArgs("-disposition:v:0", "attached_pic")
+      .addExtraArgs("-c:v:0", "mjpeg")
+      .addExtraArgs("-id3v2_version", "3")
+      .done()
 
-        try {
-            URI(fullUrl).toURL().openStream().use { input ->
-                Files.copy(input, outputFile.toPath(), StandardCopyOption.REPLACE_EXISTING)
-            }
-            lfdLogger("Cover image saved to ${outputFile.absolutePath}")
-        } catch (e: Exception) {
-            lfdLogger("Failed to download cover image: ${e.message}")
-        }
+    val executor = FFmpegExecutor(ffmpeg, ffprobe)
+    executor.createJob(builder, object : ProgressListener {
+      val durationNs = book.audiobook_info.duration * TimeUnit.SECONDS.toNanos(1)
+      override fun progress(progress: Progress) {
+        val percentage = progress.out_time_ns.toDouble() / durationNs.toDouble();
+        lfdLogger(
+          String.format(
+            "[%.0f%%] status:%s time:%s ms speed:%.2fx",
+            percentage * 100,
+            progress.status,
+            FFmpegUtils.toTimecode(progress.out_time_ns, TimeUnit.NANOSECONDS),
+            progress.speed
+          )
+        )
+      }
+    }).run()
+
+    //cleanup
+    listFile.delete()
+    metadataFile.delete()
+
+    lfdLogger("M4B file created: ${newFile.absolutePath}")
+  }
+
+  private fun downloadCoverImage(coverUrl: String, outputFile: File) {
+    val fullUrl = if (coverUrl.startsWith("//")) "https:$coverUrl" else coverUrl
+
+    try {
+      URI(fullUrl).toURL().openStream().use { input ->
+        Files.copy(input, outputFile.toPath(), StandardCopyOption.REPLACE_EXISTING)
+      }
+      lfdLogger("Cover image saved to ${outputFile.absolutePath}")
+    } catch (e: Exception) {
+      lfdLogger("Failed to download cover image: ${e.message}")
+    }
+  }
+
+  private fun generateMetadataFile(
+    book: Book,
+    chapterFiles: List<File>,
+    outputFile: File,
+  ) {
+    // Extract metadata
+    val title = book.title
+    val author = book.authors.joinToString(", ")
+    val series = book.series
+    val publisher = book.publisher
+    val publicationDate = book.publication_date
+    val genres = book.genres.map { it.name }
+    val description = book.description
+    val seriesNum = book.series_num
+
+
+    val chapters = mutableListOf<Chapter>()
+    var startTimeMs = 0L
+
+    chapterFiles.forEachIndexed { index, file ->
+      val durationMs = getMp3Duration(file)
+      val endTimeMs = startTimeMs + durationMs
+      chapters.add(Chapter("Chapter ${index + 1}", startTimeMs, endTimeMs))
+      startTimeMs = endTimeMs
     }
 
-    private fun generateMetadataFile(
-        book: Book,
-        chapterFiles: List<File>,
-        outputFile: File,
-    ) {
-        // Extract metadata
-        val title = book.title
-        val author = book.authors.joinToString(", ")
-        val series = book.series
-        val publisher = book.publisher
-        val publicationDate = book.publication_date
-        val genres = book.genres.map { it.name }
-        val description = book.description
-        val seriesNum = book.series_num
+    outputFile.bufferedWriter().use { writer ->
+      writer.write(";FFMETADATA1\n")
+      writer.write("title=\"${title}\"\n")
+      writer.write("artist=\"${author}\"\n")
+      writer.write("album=\"${title}${series?.let { " ($it Book ${seriesNum ?: "X"})" } ?: ""}\"\n")
+      writer.write("genre=\"${genres.joinToString(", ")}\"\n")
+      writer.write("date=\"${publicationDate.substring(0, 4)}\"\n")
+      writer.write("publisher=\"${publisher}\"\n")
+      writer.write("comment=\"${description.replace("\n", " ")}\"\n\n")
 
-
-        val chapters = mutableListOf<Chapter>()
-        var startTimeMs = 0L
-
-        chapterFiles.forEachIndexed { index, file ->
-            val durationMs = getMp3Duration(file)
-            val endTimeMs = startTimeMs + durationMs
-            chapters.add(Chapter("Chapter ${index + 1}", startTimeMs, endTimeMs))
-            startTimeMs = endTimeMs
-        }
-
-        outputFile.bufferedWriter().use { writer ->
-            writer.write(";FFMETADATA1\n")
-            writer.write("title=\"${title}\"\n")
-            writer.write("artist=\"${author}\"\n")
-            writer.write("album=\"${title}${series?.let { " ($it Book ${seriesNum ?: "X"})" } ?: ""}\"\n")
-            writer.write("genre=\"${genres.joinToString(", ")}\"\n")
-            writer.write("date=\"${publicationDate.substring(0, 4)}\"\n")
-            writer.write("publisher=\"${publisher}\"\n")
-            writer.write("comment=\"${description.replace("\n", " ")}\"\n\n")
-
-            chapters.forEach { chapter ->
-                writer.write("[CHAPTER]\n")
-                writer.write("TIMEBASE=1/1000\n")
-                writer.write("START=${chapter.startTimeMs}\n")
-                writer.write("END=${chapter.endTimeMs}\n")
-                writer.write("title=\"${chapter.title}\"\n\n")
-            }
-        }
+      chapters.forEach { chapter ->
+        writer.write("[CHAPTER]\n")
+        writer.write("TIMEBASE=1/1000\n")
+        writer.write("START=${chapter.startTimeMs}\n")
+        writer.write("END=${chapter.endTimeMs}\n")
+        writer.write("title=\"${chapter.title}\"\n\n")
+      }
     }
+  }
 
-    private fun getMp3Duration(file: File): Long {
-        val probeResult = ffprobe.probe(file.absolutePath)
-        val format = probeResult.format
-        return TimeUnit.MILLISECONDS.convert(format.duration.toLong(), TimeUnit.SECONDS)
-    }
+  private fun getMp3Duration(file: File): Long {
+    val probeResult = ffprobe.probe(file.absolutePath)
+    val format = probeResult.format
+    return TimeUnit.MILLISECONDS.convert(format.duration.toLong(), TimeUnit.SECONDS)
+  }
 }
